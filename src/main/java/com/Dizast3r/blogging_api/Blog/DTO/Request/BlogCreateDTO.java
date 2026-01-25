ebde9d3b5d37bfc1b83a4da86cff86747bed7f96
Dizast3r <jm2006900@gmail.com>
2026-01-24 21:31:25 -0500

    dtos añadidos

---
+++ src/main/java/com/Dizast3r/blogging_api/Blog/DTO/Request/BlogCreateDTO.java
@@ -0,0 +1,25 @@
+package com.Dizast3r.blogging_api.Blog.DTO.Request;
+
+import com.Dizast3r.blogging_api.Blog.Entities.Tag;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import java.time.LocalDateTime;
+import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class BlogCreateDTO {
+
+    @NotBlank(message = "El titulo es obligatorio")
+    @Size(max = 30, message = "El titulo no puede tener mas de 30 caracteres")
+    private String titulo;
+
+    @NotBlank(message = "El Contenido es obligatorio")
+    private String contenido;
+
+    private LocalDateTime fechaDeCreacion;
+
+    private Set<Tag> blogTags;
+}
---
+++ src/main/java/com/Dizast3r/blogging_api/Blog/DTO/Request/BlogDeleteDTO.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Dizast3r.blogging_api.Blog.DTO.Request;
+
+/**
+ *
+ * @author User
+ */
+public class BlogDeleteDTO {
+    
+}
